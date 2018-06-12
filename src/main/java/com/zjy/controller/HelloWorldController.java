e0d013adca7100c4a49ce4738102cca8787f1b6c
张伟 <zhangwei@zhangweideMBP.lan>
2018-06-12 23:19:14 +0800

    基本框架

---
+++ src/main/java/com/zjy/controller/HelloWorldController.java
@@ -0,0 +1,29 @@
+package com.zjy.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@EnableAutoConfiguration
+@RestController
+public class HelloWorldController {
+	@RequestMapping("/index")
+	public String index() {
+		return "success";
+	}
+
+	@RequestMapping("getMap")
+	public Map<String, Object> getMap() {
+		Map<String,Object> result =new HashMap<String, Object>();
+		result.put("errorCode", "200");
+		result.put("errorMsg", "成功");
+		return  result;
+
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldController.class,args);
+	}
+}
